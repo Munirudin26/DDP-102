f31154fbd5df317b5f58fa8ec82970b6113841ba
Munirudin26 <muhammadmunirudin26@gmail.com>
2025-04-02 08:42:11 +0700

    Create latihan halo dunia.cpp

---
+++ latihan halo dunia.cpp
@@ -0,0 +1,10 @@
+//program pertamaku
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"halo dunia dari debian";
+    cin.get();// menunggu user menekan enter
+    return 0;
+    
+}
